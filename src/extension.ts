997697887403c7d11e468b284b355612bb84c22f
Cole Torres <43198157+coltranetorres@users.noreply.github.com>
2026-07-24 23:40:21 +0800

    Add webview panel manager and manual preview command

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,12 @@
 import * as vscode from 'vscode';
+import { showKobeCelebration } from './popupPanel';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Kobe Git Commit extension activated.');
+  context.subscriptions.push(
+    vscode.commands.registerCommand('kobeGitCommit.previewCelebration', () => {
+      showKobeCelebration(context.extensionUri);
+    })
+  );
 }
 
 export function deactivate() {}
---
+++ src/popupPanel.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+let activePanel: vscode.WebviewPanel | undefined;
+
+export function showKobeCelebration(extensionUri: vscode.Uri): void {
+  if (activePanel) {
+    activePanel.dispose();
+  }
+
+  const mediaUri = vscode.Uri.joinPath(extensionUri, 'media');
+
+  const panel = vscode.window.createWebviewPanel(
+    'kobeGitCommit',
+    'Kobe!',
+    { viewColumn: vscode.ViewColumn.Beside, preserveFocus: true },
+    {
+      enableScripts: true,
+      localResourceRoots: [mediaUri],
+    }
+  );
+
+  activePanel = panel;
+
+  const htmlPath = vscode.Uri.joinPath(mediaUri, 'webview.html').fsPath;
+  const rawHtml = fs.readFileSync(htmlPath, 'utf8');
+
+  const cssUri = panel.webview.asWebviewUri(vscode.Uri.joinPath(mediaUri, 'webview.css'));
+  const jsUri = panel.webview.asWebviewUri(vscode.Uri.joinPath(mediaUri, 'webview.js'));
+  const audioUri = panel.webview.asWebviewUri(vscode.Uri.joinPath(mediaUri, 'kobe.mp3'));
+
+  panel.webview.html = rawHtml
+    .replace(/{{cspSource}}/g, panel.webview.cspSource)
+    .replace('{{cssUri}}', cssUri.toString())
+    .replace('{{jsUri}}', jsUri.toString())
+    .replace('{{audioUri}}', audioUri.toString());
+
+  panel.webview.onDidReceiveMessage((message: { type?: string }) => {
+    if (message?.type === 'done') {
+      panel.dispose();
+    }
+  });
+
+  panel.onDidDispose(() => {
+    if (activePanel === panel) {
+      activePanel = undefined;
+    }
+  });
+}
